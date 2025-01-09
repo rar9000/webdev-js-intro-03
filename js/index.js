288ce776104718ac6c555a19d9de2f80243ebda5
rar9000 <clavius@Clavius.local>
2025-01-09 13:38:53 -0500

    first part done

--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@
 // that are not declared
 "use strict"
 
+/*
+console.log(2 + 2);
+console.log(3 + 9 - 4 + 1 + 50 - 49);
+console.log("835" + "44" + 20);
+console.log("323" + 5 + 5)
+console.log(5 + 5 + "323")
+console.log((9 * 9) / 81);
+console.log(3242 % 3);
+console.log(43 === 42);
+console.log("1" === "2");
+console.log("-2" === "-2");
+console.log("2" === "-2")
+console.log(44 || 345345);
+console.log(-3 || 3);
+console.log(3 || -3);
+console.log(-1 && 1);
+*/
+
+
